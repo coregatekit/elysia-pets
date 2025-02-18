cd74dee94b369a497b79bb82133171634aa422e4
Joe Kim <joesm.genie@gmail.com>
2025-02-19 00:42:54 +0700

    Refactor animals module to use enums for HTTP status and add placeholder route for animal by ID

--- src/modules/animals/index.ts
+++ src/modules/animals/index.ts
@@ -1,9 +1,12 @@
 import Elysia from 'elysia';
-import { HTTP_STATUS } from '../../db/common/constants';
+import { HTTP_STATUS } from '../../db/common/enums';
 
-const Animals = new Elysia({ name: 'Animals', prefix: '/animals' }).get(
-	'/',
-	({ error }) => error(HTTP_STATUS.NOT_IMPLEMENTED, 'Not implemented'),
-);
+const Animals = new Elysia({ name: 'Animals', prefix: '/animals' })
+	.get('/', ({ error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, 'Not implemented'),
+	)
+	.get('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, 'Not implemented'),
+	);
 
 export default Animals;
