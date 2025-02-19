1ae5f337a1396589fdfd91b894eb4f5e7be3fb90
Joe Kim <joesm.genie@gmail.com>
2025-02-19 16:35:31 +0700

    Add CRUD operations for pets in the API

--- src/modules/pets/index.ts
+++ src/modules/pets/index.ts
@@ -1,13 +1,36 @@
 import Elysia from 'elysia';
 import { ERROR_MESSAGE, HTTP_STATUS } from '../../common/enums';
 
-const Pets = new Elysia({ name: 'Pets', prefix: '/pets' }).get(
-	'/',
-	({ error }) =>
+const Pets = new Elysia({ name: 'Pets', prefix: '/pets' })
+	.get('/', ({ error }) =>
 		error(HTTP_STATUS.NOT_IMPLEMENTED, {
 			status: HTTP_STATUS.NOT_IMPLEMENTED,
 			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
 		}),
-);
+	)
+	.get('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.post('/', ({ error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.patch('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.delete('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	);
 
 export default Pets;
