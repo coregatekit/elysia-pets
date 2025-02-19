821b8bb77eb94b81846b4a2da6cd128218ab15fd
Joe Kim <joesm.genie@gmail.com>
2025-02-19 16:37:44 +0700

    Add CRUD operations for owners in the API

--- src/modules/owners/index.ts
+++ src/modules/owners/index.ts
@@ -1,12 +1,36 @@
 import Elysia from 'elysia';
+import { ERROR_MESSAGE, HTTP_STATUS } from '../../common/enums';
 
-const Owners = new Elysia({ name: 'Owners', prefix: '/owners' }).get(
-	'/',
-	({ error }) =>
-		error(501, {
-			status: 501,
-			message: 'Not Implemented',
+const Owners = new Elysia({ name: 'Owners', prefix: '/owners' })
+	.get('/', ({ error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
 		}),
-);
+	)
+	.get('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.post('/', ({ error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.patch('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	)
+	.delete('/:id', ({ params: { id }, error }) =>
+		error(HTTP_STATUS.NOT_IMPLEMENTED, {
+			status: HTTP_STATUS.NOT_IMPLEMENTED,
+			message: ERROR_MESSAGE.NOT_IMPLEMENTED,
+		}),
+	);
 
 export default Owners;
